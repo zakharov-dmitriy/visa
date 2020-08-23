7ce3f03b5db8e87823f9a4d877eff7716199d96c
Дмитрий Захаров <dz@MacBook-Pro.local>
2020-08-23 16:32:51 +0700

    адаптив

--- js/main.js
+++ js/main.js
@@ -6,5 +6,29 @@ $(function () {
       centerMode: true,
       centerPadding: 60,
       slidesToShow: 3,
+      responsive: [
+         {
+            breakpoint: 770,
+            settings: {
+               slidesToShow: 1,
+               arrows: false,
+               autoplay: true,
+            }
+         }
+      ]
    });
+
+   // $('.type__list').slick({
+   //    centerMode: true,
+   //    responsive: [
+   //       {
+   //          breakpoint: 450,
+   //          settings: {
+   //             slidesToShow: 3,
+   //             slidesToScroll: 1,
+   //             infinite: true,
+   //          }
+   //       }
+   //    ]
+   // });
 })
